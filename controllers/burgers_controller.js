78df16832921bbc8a82932d26b1fe9860586dfb9
Ramon Barajas <ramonb621@yahoo.com>
2019-09-15 15:22:51 -0700

    inserted add burger form to index.handlebars

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -34,7 +34,7 @@ router.put("/api/burgers/:id", function (req, res) {
         devoured: req.body.devoured
     }, condition, function (result) {
         if (result.changedRows == 0) {
-            // If no rows were changed, then the ID must not exist, so 404
+           
             return res.status(404).end();
         } else {
             res.status(200).end();
@@ -47,7 +47,7 @@ router.delete("/api/burgers/:id", function (req, res) {
 
     burger.delete(condition, function (result) {
         if (result.affectedRows == 0) {
-            // If no rows were changed, then the ID must not exist, so 404
+            
             return res.status(404).end();
         } else {
             res.status(200).end();
