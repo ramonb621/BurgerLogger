9dbefb708fa3778789ea9847264b1c079052bdf0
Ramon Barajas <ramonb621@yahoo.com>
2019-09-14 22:42:58 -0700

    started seeds and schema sql files. Added gitignore for node_modules

---
+++ db/seeds.sql
@@ -0,0 +1,5 @@
+INSERT INTO burgers (name, devoured) VALUES ("Cheeseburger", false);
+INSERT INTO burgers (name, devoured) VALUES ("Colossal Burger", false);
+INSERT INTO burgers (name, devoured) VALUES ("Turkey Burger", false);
+INSERT INTO burgers (name, devoured) VALUES ("Veggie Burger", false);
+INSERT INTO burgers (name, devoured) VALUES ("Patty Melt", false);
