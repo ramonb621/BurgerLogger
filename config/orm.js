b505e7cd14144313b569031ba83b5af904e23257
Ramon Barajas <ramonb621@yahoo.com>
2019-09-14 23:15:15 -0700

    added routes and devour buttons

---
+++ config/orm.js
@@ -0,0 +1,6 @@
+var connection = require("../config/connection.js");
+
+//Necessary methods to execute MYSQL commmands in controllers
+//selectAll(), insertOne(), updateOne()
+
+module.exports = orm;
--- db/schema.sql
+++ db/schema.sql
@@ -5,8 +5,8 @@ CREATE DATABASE burgers_db;
 USE burgers_db;
 
 CREATE TABLE burgers (
-    id NOT NULL AUTO_INCREMENT,
+    id INT NOT NULL AUTO_INCREMENT,
     burger_name VARCHAR(50) NOT NULL,
     devoured BOOLEAN DEFAULT false,
     PRIMARY KEY (id)
-)
+);
